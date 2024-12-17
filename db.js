9d8bc131fb1a9e50ae0032b2937faa6f8fa1ab5e
jenil <jenil252525@gmail.com>
2024-12-17 10:40:24 +0530

    settings done

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
-const mysql=require("mysql2")
-                const db = mysql.createConnection({
-                    host: "localhost",
-                    user: "root",
-                    password: "1410",
-                    database:"diosync",
-                });
-                db.connect((err) => {
-                    if (err) {
-                    console.error('Error connecting to the database:', err);
-                    }
-                    console.log('Connected to the MySQL database.');
-                });
-module.exports=db
+const mysql = require("mysql2")
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Password@123",
+    database: "diosync",
+});
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Connected to the MySQL database.');
+    }
+});
+module.exports = db
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const db=require("./db")
-const express=require("express")
-const app=express()
+const db = require("./db")
+const express = require("express")
+const app = express()
 app.use(express.json())
-const cors=require("cors")
+const cors = require("cors")
 app.use(cors({
     origin: "http://localhost:3000",
-    credentials:true
+    credentials: true
 }))
 
 app.use(express.urlencoded({ extended: true }));
 const router = require("./routes/ItemRoute")
-
+const gensettingsrouter = require("./routes/generalsettings")
 
 app.use(express.urlencoded({ extended: true }));
-app.use("/",router)
-const server=app.listen(1234,()=>{
+app.use("/", router)
+app.use("/admin-panel", gensettingsrouter)
+
+
+const server = app.listen(1234, () => {
     console.log("server is running on port 1234")
 })
